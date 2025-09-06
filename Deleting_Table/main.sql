0b177f342ca3a7807b115242108cf2b49a19e58a
AbdulSamad94 <abdulsamadsiddiqui2000@gmail.com>
2025-09-06 19:45:01 +0500

    delete method added, (CRUD Operation Completed)

---
+++ Deleting_Table/main.sql
@@ -0,0 +1,3 @@
+SELECT * from person;
+
+DELETE FROM person WHERE id = 191;
--- Insert_into_Table/main.sql
+++ Insert_into_Table/main.sql
@@ -3,3 +3,8 @@ VALUES(199, 'Samad', 'Karachi');
 
 INSERT INTO person2(id, name, city)
 VALUES(200, 'Muhammed', 'Riyadh');
+
+INSERT INTO person(id, name)
+VALUES(200, 'Muhammed');
+
+

f914edfe02308f9e62c3520918c008fd8370c9d1
AbdulSamad94 <abdulsamadsiddiqui2000@gmail.com>
2025-09-06 19:39:18 +0500

    Updating the table

--- Reading_Table/main.sql
+++ Reading_Table/main.sql
@@ -1,2 +1,7 @@
 SELECT * from person;
 SELECT * from person2;
+
+
+SELECT name, city FROM person;
+
+SELECT id FROM person;
---
+++ Updating_Table/main.sql
@@ -0,0 +1,5 @@
+SELECT * from person;
+
+UPDATE person
+    SET name='Abdul Samad Siddiqui'
+    WHERE id = 191
